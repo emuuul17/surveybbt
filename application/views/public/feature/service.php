9e93fb446a09aa5b9c3fafa7602154bde34e95c6
zoe606 <rive99erlangga@gmail.com>
2019-03-09 16:56:27 +0700

    fix service fe

--- application/views/public/feature/service.php
+++ application/views/public/feature/service.php
@@ -1,84 +1,140 @@
-<section class="section py-5">
-<?php
-    $i = 0;
-    foreach($service as $serv) : $i++ ?>
-    <?php if ($i %2 !=0 ) : ?>
-    var_dump($variabel)
+    <!-- blog -->
     <div class="container pt-3">
         <div class="row py-sm-5">
+        <?php 
+        $i = 0;
+        foreach($service as $x) : $i++ ?>
+        <?php if($i %2 != 0) : ?>
             <!-- blog grid -->
            <div class="col-lg-4 col-md-6">
                 <div class="card">
                     <div class="card-header p-0">
-                        <a href="<?php echo base_url();?>index.php/antawijaya/single">
-                            <img class="card-img-bottom" src="<?php echo base_url();?>/assets/web/images/img1.jpg" alt="Card image cap">
-                        </a>
+                        <img class="card-img-bottom" src="<?= base_url();?>/assets/upload/feature/<?= $x->img ?>" alt="Card image cap">
                     </div>
                     <div class="card-body">
                         <div class="border-bottom py-2">
-                <h2 class="blog-title card-title font-weight-bold"><?= $serv->judul; ?></h2>
-                <h3> <?= $serv->sub_judul  ?></h3>
-                <p class="my-15"><?= $serv->desk_1; ?></p>
-                <p class="my-15"><?= $serv->desk__2; ?></p>
-                <p class="my-15"><?= $serv->desk__3; ?></p>
-                <p class="my-15"><?= $serv->desk__4; ?></p>
-                <p class="my-15"><?= $serv->desk__5; ?></p>
-                <p class="my-15"><?= $serv->desk__6; ?></p>
-                <p class="my-15"><?= $serv->desk__7; ?></p>
-                <p class="my-15"><?= $serv->desk__8; ?></p>
-                <p class="my-15"><?= $serv->desk__9; ?></p>
-                <p class="my-15"><?= $serv->desk__10; ?></p>
-                <p class="my-15"><?= $serv->desk__11; ?></p>
-                <p class="my-15"><?= $serv->desk__12; ?></p>
-                <p class="my-15"><?= $serv->desk__13; ?></p>
-                <p class="my-15"><?= $serv->desk__14; ?></p>
-                <p class="my-15"><?= $serv->desk__15; ?></p>
+                            <h5 class="blog-title card-title font-weight-bold"><?= $x->judul ?></h5>
+                        </div>
+                        <div class="blog_w3icon pt-4">
+                                <i class="fab fa-servicestack mr-2"></i><?= $x->sub_judul ?></span>
+                        </div>
+                        <p class="card-text mt-3">
+                            <?php if(!empty($x->desk_1)) : ?>
+                            <li> <?= $x->desk_1 ?></li>
+                            <?php endif; ?>
+                            <?php if(!empty($x->desk_2)) : ?>
+                            <li> <?= $x->desk_2 ?></li>
+                            <?php endif; ?>
+                            <?php if(!empty($x->desk_3)) : ?>
+                            <li> <?= $x->desk_3 ?></li>
+                            <?php endif; ?>
+                            <?php if(!empty($x->desk_4)) : ?>
+                            <li> <?= $x->desk_4 ?></li>
+                            <?php endif; ?>
+                            <?php if(!empty($x->desk_5)) : ?>
+                            <li> <?= $x->desk_5 ?></li>
+                            <?php endif; ?>
+                            <?php if(!empty($x->desk_6)) : ?>
+                            <li> <?= $x->desk_6 ?></li>
+                            <?php endif; ?>
+                            <?php if(!empty($x->desk_7)) : ?>
+                            <li> <?= $x->desk_7 ?></li>
+                            <?php endif; ?>
+                            <?php if(!empty($x->desk_8)) : ?>
+                            <li> <?= $x->desk_8 ?></li>
+                            <?php endif; ?>
+                            <?php if(!empty($x->desk_9)) : ?>
+                            <li> <?= $x->desk_9 ?></li>
+                            <?php endif; ?>
+                            <?php if(!empty($x->desk_10)) : ?>
+                            <li> <?= $x->desk_10 ?></li>
+                            <?php endif; ?>
+                            <?php if(!empty($x->desk_11)) : ?>
+                            <li> <?= $x->desk_11 ?></li>
+                            <?php endif; ?>
+                            <?php if(!empty($x->desk_12)) : ?>
+                            <li> <?= $x->desk_12 ?></li>
+                            <?php endif; ?>
+                            <?php if(!empty($x->desk_13)) : ?>
+                            <li> <?= $x->desk_13 ?></li>
+                            <?php endif; ?>
+                            <?php if(!empty($x->desk_14)) : ?>
+                            <li> <?= $x->desk_14 ?></li>
+                            <?php endif; ?>
+                            <?php if(!empty($x->desk_15)) : ?>
+                            <li> <?= $x->desk_15 ?></li>
+                            <?php endif; ?>
+                        </p>
+                    </div>
                 </div>
-            <div class="col-lg-6 mt-lg-0 mt-5 img">
-                <img src="<?= base_url();?>/assets/upload/feature/<?= $serv->img ?>" class="img-fluid" alt="Card image cap"/>
             </div>
-        </div>
-    </div>
-</section>
-<!-- //section -->
-<?php endif;?>
-<?php if ($i %2 == 0 ) : ?>
-<!-- section -->
-<section class="section py-5 bg-light">
-<div class="container pt-3">
-        <div class="row py-sm-5">
+            <!-- //blog grid -->
+            <?php endif;?>
+            <?php if ($i %2 == 0 ) : ?>
             <!-- blog grid -->
-           <div class="col-lg-4 col-md-6">
+           <div class="col-lg-4 col-md-6 my-md-0 my-5">
                 <div class="card">
                     <div class="card-header p-0">
-                        <a href="<?php echo base_url();?>index.php/antawijaya/single">
-                            <img class="card-img-bottom" src="<?php echo base_url();?>/assets/web/images/img1.jpg" alt="Card image cap">
-                        </a>
+                        <img class="card-img-bottom" src="<?= base_url();?>/assets/upload/feature/<?= $x->img ?>" alt="Card image cap">
                     </div>
                     <div class="card-body">
                         <div class="border-bottom py-2">
-            <h2 class="blog-title card-title font-weight-bold"><?= $serv->judul ?></h2>
-                <h3> <?= $serv->sub_judul  ?></h3>
-                <h2 class="blog-title card-title font-weight-bold"><?= $serv->judul; ?></h2>
-                <h3> <?= $serv->sub_judul  ?></h3>
-                <p class="my-15"><?= $serv->desk_1; ?></p>
-                <p class="my-15"><?= $serv->desk__2; ?></p>
-                <p class="my-15"><?= $serv->desk__3; ?></p>
-                <p class="my-15"><?= $serv->desk__4; ?></p>
-                <p class="my-15"><?= $serv->desk__5; ?></p>
-                <p class="my-15"><?= $serv->desk__6; ?></p>
-                <p class="my-15"><?= $serv->desk__7; ?></p>
-                <p class="my-15"><?= $serv->desk__8; ?></p>
-                <p class="my-15"><?= $serv->desk__9; ?></p>
-                <p class="my-15"><?= $serv->desk__10; ?></p>
-                <p class="my-15"><?= $serv->desk__11; ?></p>
-                <p class="my-15"><?= $serv->desk__12; ?></p>
-                <p class="my-15"><?= $serv->desk__13; ?></p>
-                <p class="my-15"><?= $serv->desk__14; ?></p>
-                <p class="my-15"><?= $serv->desk__15; ?></p>
-        </div>
-    </div>
-    <?php endif;?>
+                            <h5 class="blog-title card-title font-weight-bold"><?= $x->judul ?></h5>
+                        </div>
+                        <div class="blog_w3icon pt-4">
+                                <i class="fab fa-servicestack mr-2"></i><?= $x->sub_judul ?></span>
+                        </div>
+                        <p class="card-text mt-3">
+                        <?php if(!empty($x->desk_1)) : ?>
+                            <li> <?= $x->desk_1 ?></li>
+                            <?php endif; ?>
+                            <?php if(!empty($x->desk_2)) : ?>
+                            <li> <?= $x->desk_2 ?></li>
+                            <?php endif; ?>
+                            <?php if(!empty($x->desk_3)) : ?>
+                            <li> <?= $x->desk_3 ?></li>
+                            <?php endif; ?>
+                            <?php if(!empty($x->desk_4)) : ?>
+                            <li> <?= $x->desk_4 ?></li>
+                            <?php endif; ?>
+                            <?php if(!empty($x->desk_5)) : ?>
+                            <li> <?= $x->desk_5 ?></li>
+                            <?php endif; ?>
+                            <?php if(!empty($x->desk_6)) : ?>
+                            <li> <?= $x->desk_6 ?></li>
+                            <?php endif; ?>
+                            <?php if(!empty($x->desk_7)) : ?>
+                            <li> <?= $x->desk_7 ?></li>
+                            <?php endif; ?>
+                            <?php if(!empty($x->desk_8)) : ?>
+                            <li> <?= $x->desk_8 ?></li>
+                            <?php endif; ?>
+                            <?php if(!empty($x->desk_9)) : ?>
+                            <li> <?= $x->desk_9 ?></li>
+                            <?php endif; ?>
+                            <?php if(!empty($x->desk_10)) : ?>
+                            <li> <?= $x->desk_10 ?></li>
+                            <?php endif; ?>
+                            <?php if(!empty($x->desk_11)) : ?>
+                            <li> <?= $x->desk_11 ?></li>
+                            <?php endif; ?>
+                            <?php if(!empty($x->desk_12)) : ?>
+                            <li> <?= $x->desk_12 ?></li>
+                            <?php endif; ?>
+                            <?php if(!empty($x->desk_13)) : ?>
+                            <li> <?= $x->desk_13 ?></li>
+                            <?php endif; ?>
+                            <?php if(!empty($x->desk_14)) : ?>
+                            <li> <?= $x->desk_14 ?></li>
+                            <?php endif; ?>
+                            <?php if(!empty($x->desk_15)) : ?>
+                            <li> <?= $x->desk_15 ?></li>
+                            <?php endif; ?>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <?php endif;?>
     <?php endforeach; ?>
-    
-</section>
+        </div>
+    </div> 
