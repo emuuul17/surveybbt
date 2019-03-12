ee1d406db697a11e4cc271df8505bfd2e5b1c78a
zoe606 <rive99erlangga@gmail.com>
2019-03-12 20:51:03 +0700

    fix page

--- application/views/public/target/page.php
+++ application/views/public/target/page.php
@@ -5,7 +5,7 @@
 			<div class="col-lg-12 mb-lg-0 mb-5">
                 <h3 class="mb-3 text-capitalize text-center"><?= $item->judul ?></h3>
                 <div class="col-lg-12 text-center">
-				<img src="<?= base_url();?>/assets/upload/target/<?= $item->img ?>" alt="default.jpg" class="img-fluid" style="max-width: 70%">
+				<img src="<?= base_url();?>/assets/upload/target/<?= $item->img ?>" alt="default.png" class="img-fluid" style="max-width: 70%">
 			</div>
 				<p class="mb-3 mt-3 text-justify"><?= $item->deskripsi  ?></p>
 				</div>
