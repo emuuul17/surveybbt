9dee6dbfb63d7ab4351d77295965081bfd1cbcc0
zoe606 <rive99erlangga@gmail.com>
2019-03-12 21:30:11 +0700

    about welcome be and fix fe

--- application/views/admin/_partials/sidebar.php
+++ application/views/admin/_partials/sidebar.php
@@ -3,6 +3,19 @@
       <li class="nav-item active <?= $this->uri->segment(2) == '' ? 'active': '' ?>">
           <?= anchor('admin', ' Home', array('class' => 'nav-link fas fa-fw fa-tachometer-alt'));?>
       </li>
+
+      <li class="nav-item dropdown active">
+        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="far fa-star"></i>
+          <span>About</span>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+          <h6 class="dropdown-header">About:</h6>
+          <?= anchor('admin/about/welcome', ' Welcome', array('class' => 'dropdown-item'));?>
+          <?= anchor('admin/feature/service', ' Services', array('class' => 'dropdown-item'));?>
+        </div>
+      </li>
+
       <!-- <li class="nav-item">
         <?= anchor('admin/pages', ' Pages', array('class' => 'nav-link fas fa-fw fa-chart-area'));?>
       </li> -->
---
+++ application/views/admin/about/welcome.php
@@ -0,0 +1,85 @@
+<div class="row">
+	<div class="col-md-12">
+    <?php if($this->session->flashdata('success')) : ?>
+        <?= '<div class="alert alert-dismissable alert-success">'.$this->session->flashdata('success').'</div>'  ?>
+    <?php endif; ?>
+    <?php if($this->session->flashdata('error')) : ?>
+        <?= '<div class="alert alert-dismissable alert-danger">'.$this->session->flashdata('error').'</div>'  ?>
+    <?php endif; ?>
+
+		<div class="card mb-3">
+        <?= form_open('admin/about/welcome/'. $welcome->id); ?>
+
+			<div class="card-header text-white bg-primary"> <i class="fas fa-list"></i><b><?= $title ?></b> </div>
+
+			<div class="card-body">
+				<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+					<li class="nav-item"><a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab"
+						 aria-controls="pills-general" aria-selected="true"><?= $title ?></a></li>
+				</ul>
+
+
+				<div class="tab-content" id="pills-tabContent">
+					<div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+					   
+                        <div class="form-group">
+                        <?= form_label('Judul', 'judul');  ?>
+                            <?php
+                                $data = array(
+                                    'name' => 'judul',
+                                    'id' => 'judul',
+                                    'maxlength' => '50',
+                                    'class' => 'form-control',
+                                    'value' => $welcome->judul
+                                );  
+                            ?>
+                            <?= form_input($data); ?>
+                            </div>
+
+						<div class="form-group">
+                        <?= form_label('Deskripsi', 'deskripsi');  ?>
+                            <?php
+                                $data = array(
+                                    'name' => 'deskripsi',
+                                    'id' => 'deskripsi',
+                                    'maxlength' => '100',
+                                    'class' => 'form-control',
+                                    'value' => $welcome->deskripsi
+                                );  
+                            ?>
+                            <?= form_textarea($data); ?>
+                            </div>
+
+                            <div class="row">
+
+                                <div class="form-group col-md-6">
+                                <input type="hidden" name="old_img" value="<?= $welcome->img ?>" >
+                                    <img src="<?= base_url();?>/assets/upload/welcome/<?= $welcome->img ?>" class="img-fluid" alt="Card image cap"/>
+                                </div>
+
+                                <div class="form-group col-md-6">
+                                <?= form_label('Foto', 'img');  ?>
+                                <?php
+                                    $data = array(
+                                        'name' => 'img',
+                                        'id' => 'img',
+                                        'class' => 'form-control',
+                                        'value' => set_value('img')
+                                    );  
+                                ?>
+                                <?= form_upload($data); ?>
+                                </div>
+
+                            </div>
+
+
+				    </div>
+				</div>
+
+			</div>
+
+			<div class="card-footer bg-transparent"><?= form_submit('mysubmit', 'Save', array('class' => 'btn btn-primary'));  ?></div> 
+            <?= form_close(); ?>
+		</div>
+	</div>
+</div>
