880f152b2a7bc5747066d7f95042dca2cd1e01f8
zoe606 <rive99erlangga@gmail.com>
2019-03-12 21:50:29 +0700

    fix fe

--- application/views/public/about/index.php
+++ application/views/public/about/index.php
@@ -18,7 +18,7 @@
 </section>
 
 
-<section class="contact py-5 my-lg-5">
+<section class="process py-5 my-lg-5">
 	<div class="container">
 	<div class="row contact_information">
 		<?php foreach($vmisi as $vm) : ?>
@@ -26,7 +26,7 @@
 				<div class="mt-5 information">
 					<div class="row">
 						<div class="col-sm-2 mb-sm-0 mb-3">
-							<i class="fas fa-comments"></i>
+							<i class="fas fa-star"></i>
 						</div>
 			<div class="col-sm-10">
 				<h3 class="heading text-left text-capitalize font-weight-bold mb-5"><?=$vm->judul?></h3>
