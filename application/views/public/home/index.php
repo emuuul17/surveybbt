04653d1c48c3662ad13f75bdbf9811784980c98b
Ayu Permata Sari <16066107+ayypermata@users.noreply.github.com>
2019-03-13 14:16:07 +0700

    daftar

--- application/views/public/home/index.php
+++ application/views/public/home/index.php
@@ -58,6 +58,8 @@
 	</div>
 </section>
 <!-- Testimonials -->
+
+
 	<!-- testimonials -->
 	<div class="testimonials py-5" id="testi">
 		<div class="container pb-xl-5 pb-lg-3">
@@ -88,13 +90,10 @@
 		</div>
 	</div>
 	<!-- //testimonials -->
-
-
-
 	<!-- project -->
 <section class="project py-5 text-center">
 	<div class="container">
-		<h3 class="text-capitalize mb-5">BERGABUNG BERSAMA KAMI?</h3>
+		<h3 class="text-capitalize mb-5">BERGABUNG BERSAMA KAMI??</h3>
 		<a href="<?php echo base_url();?>index.php/register" class="text-uppercase"><i></i>DAFTAR </a>
 	</div>
 </section>
