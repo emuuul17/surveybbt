91ef4f139517ad5d20af659fe6a4e8c89da8f5ed
Ayu Permata Sari <16066107+ayypermata@users.noreply.github.com>
2019-03-14 21:00:22 +0700

    daftar

--- application/views/public/home/index.php
+++ application/views/public/home/index.php
@@ -94,7 +94,7 @@
 <section class="project py-5 text-center">
 	<div class="container">
 		<h3 class="text-capitalize mb-5">BERGABUNG BERSAMA KAMI??</h3>
-		<a href="<?php echo base_url();?>index.php/register" class="text-uppercase"><i></i>DAFTAR </a>
+		<a href="<?php echo base_url();?>index.php/register" class="text-uppercase"><i></i>DAFTAR KUY HAHAA </a>
 	</div>
 </section>
 <!-- //project -->
