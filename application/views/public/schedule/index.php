77229e0a5800f1bd06b47b68fe0d05a75669e193
Ayu Permata Sari <16066107+ayypermata@users.noreply.github.com>
2019-03-27 15:48:54 +0700

    jadwal

--- application/views/public/schedule/index.php
+++ application/views/public/schedule/index.php
@@ -4,6 +4,25 @@
 	<h3 class="heading text-center text-capitalize font-weight-bold mb-5">J A D W A L </h3>
     <div class="table-responsive">          
     <table class="table table-hover">
+		<thead></thead>
+		<td>Pilih Bulan</td>
+     <td><select name="bulan" id="bulan">
+     <option value="">Pilih Bulan</option>
+		 <option value="januari">Januari</option>
+		 <option value="februari">Februari</option>
+		 <option value="maret">Maret</option>
+		 <option value="april">April</option>
+		 <option value="mei">Mei</option>
+		 <option value="juni">Juni</option>
+		 <option value="juli">Juli</option>
+		 <option value="agustus">Agustus</option>
+		 <option value="september">September</option>
+		 <option value="oktober">Oktober</option>
+		 <option value="november">November</option>
+		 <option value="november">Desember</option>
+     </select> <font color="#FF0000"><?php echo form_error('bulan');?></font></td>
+     </>
+
 		<thead-bold>
 		     <tr>
 				<th>JUDUL</th>
